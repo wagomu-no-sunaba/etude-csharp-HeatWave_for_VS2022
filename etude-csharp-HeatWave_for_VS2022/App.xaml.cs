40a1b608512f2a1606846cd8707e44c23fa45e5f
staticWagomU <to6wa17@gmail.com>
2024-04-05 21:33:36 +0900

    プロジェクト ファイルを追加します。

---
+++ etude-csharp-HeatWave_for_VS2022/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace etude_csharp_HeatWave_for_VS2022
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
